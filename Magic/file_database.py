2f21fe13389a581ddda6fbb2e6e253cd584a787c
Georgerahul24 <Georgerahul24@users.noreply.github.com>
2021-08-25 11:58:08 +0000

    Automated autoyapf fixes

--- Magic/file_database.py
+++ Magic/file_database.py
@@ -1,10 +1,10 @@
-
-
 '''This module deals with adding and verifying usernames'''
 import os
 import json
 #Get the path of users.elsa
 userpth = os.getcwd() + '\\resources\\ users.elsa'
+
+
 def check_user_from_file(username):
     """[This extension is used to check if the user is vaid or not ]
 
@@ -15,15 +15,14 @@ def check_user_from_file(username):
         [str]: [Returns the passwprd of user if found]
     """
     try:
-       
+
         file = open(userpth, 'r')
-        data=json.load(file)
-        part2=data.get(username,None)
+        data = json.load(file)
+        part2 = data.get(username, None)
         file.close()
         return part2
     except Exception as e:
         print('It seems that some error has happened', e)
-
 
 
 def write_to_file(username, password):
@@ -37,20 +36,19 @@ def write_to_file(username, password):
         [int]: [1 if it is a success and -1 if the process is a failure]
     """
     try:
-        
-        file = open(userpth,'r')
-        data=json.load(file)
+
+        file = open(userpth, 'r')
+        data = json.load(file)
         print(file)
         file.close()
-        file = open(userpth,'w')
+        file = open(userpth, 'w')
         if len(username) != 0 and username not in [
                 'initial', 'cache', 'users', 'user', 'theme', 'indexer',
                 'resources'
         ]:
 
-
-            data[username]=password
-            json.dump(data,file)
+            data[username] = password
+            json.dump(data, file)
             file.close()
             print(f"Added user {username} ")
             #returns state = 1 so that program knows that writing was succesful
--- Magic/initial_setup.py
+++ Magic/initial_setup.py
@@ -22,6 +22,6 @@ def install_files():
     # writing the users folder with default user admin and default password 1234
     userpth = os.getcwd() + "\\resources\\ users.elsa"
     f = open(userpth, 'w')
-    json.dump({'admin':"1234"},f)
+    json.dump({'admin': "1234"}, f)
     f.close()
     print("added file 'initial.Elsa'")
