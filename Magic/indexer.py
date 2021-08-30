85dc89ef8f8c73f70e10dbc69a925bf485a089d1
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-08-30 17:03:21 +0530

    Removed file prediction
    Removed file prediction as it was broken

--- Magic/indexer.py
+++ Magic/indexer.py
@@ -1,17 +1,14 @@
 import os, webbrowser
 from talk1 import talk1
-from difflib import get_close_matches
+
 from pathlib import Path
 
 indexerpth = os.getcwd() + f"\\resources\\ indexer.elsa"
 # get path of the current file os.getcwd
 # convert it into path use path(os.getcwd) use is_file() to check if it is a file
-desktop = Path(os.path.join(os.path.join(os.environ["USERPROFILE"]),
-                            "Desktop"))
-documents = Path(
-    os.path.join(os.path.join(os.environ["USERPROFILE"]), "Documents"))
-downloads = Path(
-    os.path.join(os.path.join(os.environ["USERPROFILE"]), "Downloads"))
+desktop = Path(os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop"))
+documents = Path(os.path.join(os.path.join(os.environ["USERPROFILE"]), "Documents"))
+downloads = Path(os.path.join(os.path.join(os.environ["USERPROFILE"]), "Downloads"))
 music = Path(os.path.join(os.path.join(os.environ["USERPROFILE"]), "Music"))
 videos = Path(os.path.join(os.path.join(os.environ["USERPROFILE"]), "Videos"))
 directories = [desktop, documents, downloads, music, videos]
@@ -21,8 +18,7 @@ def indexer_folders():
     try:
         import json
 
-        folderpth = indexerpth = os.getcwd(
-        ) + f"\\resources\\ indexerpaths.elsa"
+        folderpth = indexerpth = os.getcwd() + f"\\resources\\ indexerpaths.elsa"
         f = open(folderpth)
         folders = json.load(f)
         f.close()
@@ -50,8 +46,7 @@ def index(pathn):
                 cache.write(f"{name} @#$%^& {i} @#$%^& \n")
 
             else:  # check for the files in a directories by calling the function recursively
-                if name.startswith(".") == False and name.startswith(
-                        "__") == False:
+                if name.startswith(".") == False and name.startswith("__") == False:
                     try:
                         index(i)
                     except Exception as e:
@@ -98,13 +93,13 @@ def search_indexed_file(filename):
             data = data.split(" @#$%^& ")
             cachedict[data[0]] = data[1]
             filenames.append(data[0])
-        approx_file = get_close_matches(filename, filenames, n=1, cutoff=0.7)
+        approx_file = filename
 
         if len(approx_file) != 0:
-            srched_filepath = cachedict[approx_file[0]]
+            srched_filepath = cachedict[approx_file]
             webbrowser.open(srched_filepath)
             print(f"Opened {srched_filepath}")
-            talk1.talk(f"Opened {approx_file[0]}")
+            talk1.talk(f"Opened {approx_file}")
         else:
             print(f"Could not find any files")
             talk1.talk(f"Could not find any files")
@@ -127,8 +122,7 @@ def add_indexer_folders(event="", path=""):
     except:
         import json
 
-        folderpth = indexerpth = os.getcwd(
-        ) + f"\\resources\\ indexerpaths.elsa"
+        folderpth = indexerpth = os.getcwd() + f"\\resources\\ indexerpaths.elsa"
         f = open(folderpth, "w")
         json.dump([path], f)
         f.close()
@@ -143,8 +137,7 @@ def read_indexer_folders(event=""):
     try:
         import json
 
-        folderpth = indexerpth = os.getcwd(
-        ) + f"\\resources\\ indexerpaths.elsa"
+        folderpth = indexerpth = os.getcwd() + f"\\resources\\ indexerpaths.elsa"
         f = open(folderpth)
         folders = json.load(f)
         f.close()
