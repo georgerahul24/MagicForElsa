f9aaa164691af1f6e811dfe1fed42ac78c98c8e0
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-09-06 17:59:18 +0530

    tweaks to chat_client module
    - Added a close connection function
    - Disabled garbage collection in order to it accidently deleting the tkinter thread in multi processing

--- Magic/chat_client.py
+++ Magic/chat_client.py
@@ -2,12 +2,13 @@ import json
 import socket
 import threading
 from tkinter import Tk, Label, Button
+import gc
 
 host = '127.0.0.1'
 port = 24094
-# SOCK_STREAM. AF_INET refers to the address-family ipv4. The SOCK_STREAM means connection-oriented TCP protocol.
+# SOCK_STREAM. AF_INET refers to the address-family ipv4. The SOCK_STREAM means connection-oriented TCP protocol.(geek for geeks)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+
 
 nickname = ""
 
@@ -24,29 +25,37 @@ def recievefromserver():
             if msg == "NICK":
                 client.send(nickname.encode('ascii'))
             else:
+                gc.disable()
                 print('msg recieved', msg)
                 msgWindow = Tk()
                 Label(msgWindow, text=msg).pack()
 
                 def closewindow(event=''):
                     msgWindow.destroy()
+                    del msgWindow
 
-                Button(msgWindow, command=closewindow)
+
                 msgWindow.mainloop()
+
+                gc.enable()
         except:
-            print('An error occured')
+            print('Closing Connection')
             client.close()
             break
 
 
 def sendtoserver(nickname, msg):
     try:
+        print("Sending",msg,"to",nickname)
         msg = json.dumps((nickname, msg))
         client.send(msg.encode('ascii'))
+        print("GC",gc.isenabled())
     except:
         pass
-
+def closeClient():
+    client.close()
 
 def startclient():
+    client.connect((host, port))
     recievethread = threading.Thread(target=recievefromserver)
     recievethread.start()
