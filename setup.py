c6771964cd973b1c4145a7f6ca0ab04d45a0c639
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-10-13 16:26:25 +0530

    removed task and talk packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="magicForElsa",
-    packages=["Magic", "task1", "talk1"],
+    packages=["Magic"],
     version="1.17",
     license="GPLv3+",
     description="A package for the personal assistant Elsa",
@@ -10,7 +10,7 @@ setup(
     author_email="georgerahul24@gmail.com",
     url="https://github.com/georgerahul24/MagicForElsa",
     keywords=["program", "automate", "task", "magic"],
-    install_requires=["pyttsx3", "win10toast", "PyQt5"],
+    install_requires=["pyttsx3", "win10toast", "PyQt5","task1","talk1"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
