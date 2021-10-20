a8ca4b0b58b167ca8d0158a908551d15ed6ff46b
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2021-10-20 12:45:56 +0530

    refactoring export import

--- Magic/export_import.py
+++ Magic/export_import.py
@@ -1,4 +1,3 @@
-import gc
 import json
 
 from Magic import theme, indexer
@@ -13,83 +12,47 @@ def export() -> None:
     f = filedialog.asksaveasfile(mode="w", defaultextension=".json")
     if f is not None:
         try:
-            print("Starting to save file in", f)
-            # theme
-            themedata = theme.read_theme()
-
-            # indexer
-            indexerdata = indexer.read_indexer_folders()
-            if indexerdata is None:
-                indexerdata = None
-
             USERNAMEPATH = getcwd() + "\\resources\\ users.elsa"
             with open(USERNAMEPATH) as usernamefile:
                 username_data = json.load(usernamefile)
-
-            data = {
-                "indexfolders": indexerdata,
-                "theme": themedata,
-                "usernames": f"{username_data}",
-            }
-            json.dump(data, f, indent=4)
-            print("Successfully exported the data")
+            json.dump({"indexfolders": indexer.read_indexer_folders(), "theme": theme.read_theme(),"usernames": f"{username_data}"}, f, indent=4)
             f.close()
-            del f, themedata, indexerdata, USERNAMEPATH
-            gc.collect()
+            print("Successfully exported the data")
+            del f, USERNAMEPATH
         except Exception as e:
             print("Some error happened", e)
+    else:
+        print("No file selected")
 
 
-def import_data()->None:
+def import_data() -> None:
     """This function is to import the data"""
     try:
         from tkinter import filedialog
         from os import getcwd, remove
-
-        initpth = getcwd() + "\\resources\\ initial.elsa"
-        indexerpth = getcwd() + "\\resources\\ indexerpaths.elsa"
+        initpth, indexerpth = (getcwd() + "\\resources\\ initial.elsa"), (getcwd() + "\\resources\\ indexerpaths.elsa")
         f = filedialog.askopenfile(mode="r", defaultextension=".json")
         if f is not None:
             data = json.load(f)
-            indexdata = data.get("indexfolders")
-            themedata = data.get("theme")
-            usernamedata = data.get("usernames")
-            print(indexdata, themedata)
+            indexdata, themedata, usernamedata = data.get("indexfolders"), data.get("theme"), data.get("usernames")
             if indexdata is not None:
                 with open(indexerpth, "w") as indexfile:
                     json.dump(indexdata, indexfile, indent=4)
-                print("Imported the additional indexed folders")
                 try:
-                    removepth = getcwd() + "\\resources\\ indexer.elsa"
-                    remove(removepth)
-                    print("Deleted 'indexer.elsa'")
-                    del removepth
+                    # To rebuilt the indexes
+                    remove((getcwd() + "\\resources\\ indexer.elsa"))
                 except:
                     pass
             with open(initpth, "w") as themefile:
-                themefile.write(
-                    f"{themedata[0]};{themedata[1]};{themedata[2]}")
-            print("Imported the themes")
+                themefile.write(f"{themedata[0]};{themedata[1]};{themedata[2]}")
             USERNAMEPATH = getcwd() + "\\resources\\ users.elsa"
-
             # see https://stackoverflow.com/questions/39491420/python-jsonexpecting-property-name-enclosed-in-double-quotes
-            # json doesn't allow single qoutes. Only allows double qoutes
-            usernamedata = usernamedata.replace("'", '"')
-            usernamedata = json.loads(usernamedata)
+            # json doesn't allow single quotes. Only allows double qoutes
+            usernamedata = json.loads(usernamedata.replace("'", '"'))
             with open(USERNAMEPATH, "w") as userfile:
                 json.dump(usernamedata, userfile, indent=4)
             print("Imported usernames")
             f.close()
-            del (
-                initpth,
-                indexerpth,
-                f,
-                data,
-                indexdata,
-                themedata,
-                usernamedata,
-                USERNAMEPATH,
-            )
+            del initpth, indexerpth, f, data, indexdata, themedata, usernamedata, USERNAMEPATH
     except Exception as e:
         print("Some error happened", e)
-        del e
